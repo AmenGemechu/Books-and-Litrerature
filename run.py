58d7b45d13affb53d99286d4e774ee178070df1c
AmenGemechu <>
2023-04-26 14:32:02 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to Trivia Quiz!")
+username = input("Insert Username: ")
+print("hey", username, "\nFor every correct answear you will get one point!")
+
+def new_game():
+    pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
+questions = {
+    
+}
