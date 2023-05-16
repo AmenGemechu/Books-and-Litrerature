ffffb35784a6fce670230a7e4a4d480d40c84fcf
AmenGemechu <>
2023-05-16 18:27:39 +0000

    Remove unwanted comments and edit questions.

--- run.py
+++ run.py
@@ -61,10 +61,7 @@ def get_user_name():
             line_count += 1
 
         if found is True:
-            print(f"match in raw: {+ line_count}")
             last_score = row[1]
-            print(f'\t{row[0]} {row[1]}.')
-            print(f'Processed {line_count} lines.')
             csv_file.close()
 
         if found is False:
@@ -144,7 +141,11 @@ def play_again():
 
         with open("userdata.txt") as csv_file:
             file_content = csv_file.read()
-            replaced_content = file_content.replace(str(name) + ":" + str(last_score), str(name) + ":" + str(combined_score))
+            replaced_content = \
+                file_content.replace(
+                    str(name) + ":" + str(last_score), str(name) +
+                    ":" + str(combined_score)
+                    )
         csv_file.close()
 
         with open("userdata.txt", 'w') as csv_file:
@@ -171,13 +172,6 @@ def display_score(correct_guesses, guesses):
     print("____________________________________________")
     print_slow("RESULTS")
     print("____________________________________________")
-
-    # Displays all the values within the dictionary.
-    for i in questions:
-        print(questions.get(i), end=" ")
-    print()
-
-    print("____________________________________________")
     print("Your Answer: ", end="")
     for i in guesses:
         print(i, end=" ")
@@ -187,23 +181,24 @@ def display_score(correct_guesses, guesses):
     score = int((correct_guesses/len(questions))*100)
     print("____________________________________________")
     print("Your score is: "+str(score)+"%")
+    print("____________________________________________")
 
 
 # Storing questions & correct answers using dictionary keys and values.
 questions = {
     "1. Which horror author penned the apocalyptic novel 'The Stand'? ": "A",
-    "2. Which book is about a band of rabbits became a bestseller in 1972? ": "B",
-    "3. The classic 1877 novel 'Black Beauty' is about what kind of animal? ": "A",
-    "4. Who was the first author to use a 'typemachine' or typewriter in writing a manuscript?": "C",
-    "5. What is 1988 book by Salman Rushdie is considered blasphemous by many Muslim countries?": "C",
-    "6. Which mystery writer holds the Guinness World Record for the most translated works?": "B",
-    "7. What book holds the record for the fastest selling book in history? ": "A",
+    "2. A book is about a band of rabbits became a bestseller in 1972? ": "B",
+    "3. The classic 1877 novel 'Black Beauty' is about which animal? ": "A",
+    "4. Who was the 1st author to use a typewriter to write manuscript?": "C",
+    "5. What was the first novel ever written?": "C",
+    "6. What was the first book published by movable type?": "B",
+    "7. Which book was the fastest selling book in history?": "A",
     "8. 'Call me Ishmael' is the first line from what classic novel?": "B",
-    "9. What Charles Dickens novel begins with the sentence, 'It was the best of times, it was the worst of times'": "C",
-    "10. What popular young adult book series sends 'tributes' to participate in a televised competition in which they fight to the death? ": "C",
-    "11. Jacob Black is a character in what Stephenie Meyer book series? ": "A",
-    "12. What Nicholas Sparks book was about a young socialite and her long-time crush was made into a movie starring Ryan Gosling and Rachel McAdams?": "B",
-    "13. In one of the most popular Dr. Seuss books, what won't Sam-I-Am eat? ": "B"
+    "9. What was Stephen Kings first published novel?": "C",
+    "10. When was the first Harry Potter book published?": "C",
+    "11. Jacob Black is a character in what Stephenie Meyer book? ": "A",
+    "12. When was the New York Review Of Books first published?": "B",
+    "13. In one of Dr. Seuss books, what won't Sam-I-Am eat? ": "B"
 }
 
 # Storing multiple choice options using lists.
@@ -212,16 +207,14 @@ options = [
     ["A. The Bug Book", "B. Watership Down", "C. The Sinking Spell"],
     ["A. Horse", "B. Dog", "C. Cow"],
     ["A. Jane Auston", "B. Geore Orwell", "C. Mark Twain"],
-    ["A. The Da Vinch code", "B. The Fatal Lozenge", "C. The Satanic Verses"],
-    ["A. Pablo Coelho", "B. Agatha Christie", "C. Carlo Collodi"],
-    ["A. Harry Potter and the Deathly Hallows",
-        "B. The Lord of the Rings", "C. The Alchemist"],
-    ["A. To kill a mockingbird", "B. Moby Dick", "C. The great Gatsby"],
-    ["A. The Pickwick Papers", "B. A Christmas Carol",
-        "C. A Tale of Two Cities"],
+    ["A. The Da Vinch code", "B. The Fatal Lozenge", "C. The Tale of Genji"],
+    ["A. The 1st Bible", "B. the Gutenberg Bible", "C. The 1st English Bible"],
+    ["A. Harry Potter", "B. The Lord of the Rings", "C. The Alchemist"],
+    ["A. To kill a mockingbird", "B. Moby Dick", "C. Carrie"],
+    ["A. 1990, B. 1995 C. 1997"],
     ["A. The host", "B. The chemist", "C. The Hunger Games"],
     ["A. Twilight", "B. Speed", "C.Divergent"],
-    ["A. Shotgun Wedding", "B. The Notebook", "C. Purple hearts"],
+    ["A. 1960", "B. 1963", "C. 1970"],
     ["A. Eggs and becon", "B. Green eggs and ham", "C. Pancake"],
 ]
 
@@ -232,11 +225,12 @@ print_slow("Welcome to Books & Litrature Quiz!")
 print_slow("Please enter your User name:")
 name = get_user_name()
 print_slow(f"Hello there, {name}!")
+
 try:
     last_score
     print_slow(f"Your last Score was, {last_score}")
 except:
-    pass
+    print()
 
 new_game()
 
